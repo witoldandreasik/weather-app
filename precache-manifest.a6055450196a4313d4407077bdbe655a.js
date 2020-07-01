b0c1a4630fb33ea55cd37e953d5f0d54bc8b4257
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-01 10:57:30 +0200

    Updates

---
+++ precache-manifest.a6055450196a4313d4407077bdbe655a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "85ac30e33eb98a478d49",
+    "url": "/weather-app/static/js/main.578731b2.chunk.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/weather-app/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "85ac30e33eb98a478d49",
+    "url": "/weather-app/static/css/main.b6232abf.chunk.css"
+  },
+  {
+    "revision": "7fd7ddef28ef6824b977c54e1b286905",
+    "url": "/weather-app/index.html"
+  }
+];
